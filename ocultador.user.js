baef1c597f938675a30bfd1df2c82291484b3583
brcrepo <el.info.mail@gmail.com>
2016-11-17 03:18:21 -0300

    corrección de filtrado al llamar siguiente pagina
    corrección de error que muestra post koreanos al cargar la segunda pagina

--- ocultador.user.js
+++ ocultador.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         chw - ocultador de post koreanos
 // @namespace    chw.korea.orochimaru
-// @version      0.1
+// @version      0.2
 // @description  oculta los posts koreanos del foro y le da scroll infinito
 // @author       BRC
 // @match        http://www.chw.net/foro/*
@@ -13,19 +13,21 @@ var proximaPag = getNextPage(document);
 function filtrar(pagina){
 
     var posts = $(pagina).find('ol.threads li.threadbit');
+    var postFiltrados = [];
 
-    $(posts).each(function(){
+    $(posts).each(function(index,value){
 
         var titulo = $(this).find('a.title').text();
         var isKoreano = titulo.match(/[\u1100-\u11FF|\u3130-\u318F|\uA960-\uA97F|\uAC00-\uD7AF|\uD7B0-\uD7FF]+/g);
 
         if(isKoreano){
-
             this.remove();
+        }else{
+            postFiltrados.push(this);
         }
     });
 
-    return posts;
+    return postFiltrados;
 
 }
 
