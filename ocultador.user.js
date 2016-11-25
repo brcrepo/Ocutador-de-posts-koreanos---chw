6f2e36e6fd577516dbc4d68cbb3b8b92943ddbfb
brcrepo <el.info.mail@gmail.com>
2016-11-25 11:33:00 -0300

    ahora se filtran los resultados de las busquedas
    se filtran los resultados de las búsquedas y se les da scroll infinito
    regex koreana simplificada

--- ocultador.user.js
+++ ocultador.user.js
@@ -1,28 +1,30 @@
 // ==UserScript==
 // @name         chw - ocultador de post koreanos
 // @namespace    chw.korea.orochimaru
-// @version      0.3
-// @description  oculta los posts koreanos del foro y le da scroll infinito
+// @version      0.4
+// @description  oculta los posts koreanos/chinos del foro y le da scroll infinito
 // @author       BRC
 // @match        http://www.chw.net/foro/*
 // @require      http://code.jquery.com/jquery-latest.js
 // @grant        none
 // ==/UserScript==
 var proximaPag = getNextPage(document);
+var contenedorDePost = window.location.href.match("www.chw.net/foro/search.php") ? 'ol.searchbits ':'ol.threads ';
 
 function filtrar(pagina){
 
-    var posts = $(pagina).find('ol.threads li.threadbit');
+    var posts = $(pagina).find(contenedorDePost+ 'li.threadbit');
     var postFiltrados = [];
 
     $(posts).each(function(index,value){
 
         var titulo = $(this).find('a.title').text();
-        var isKoreano = titulo.match(/[\u1100-\u11FF|\u3130-\u318F|\uA960-\uA97F|\uAC00-\uD7AF|\uD7B0-\uD7FF]+/g);
+        var isKoreano = titulo.match(/[\uAC00-\uD7AF]+/g);
         var isChino = titulo.match(/[\u4e00-\u9fff]+/g);
 
         if(isKoreano || isChino ){
-            
+
+            console.log(titulo);
             $(this).remove();
 
         }else{
@@ -57,12 +59,11 @@ $(function(){
 
                     proximaPag= getNextPage(html);
                     var postDeProximaPagina  = filtrar(html);
-                    $(postDeProximaPagina).hide().appendTo('ol.threads').fadeIn("slow");
+                    $(postDeProximaPagina).hide().appendTo(contenedorDePost).fadeIn("slow");
 
                 },
                 complete: function(){ $(window).on("scroll",scrollHand); }
             });
-
 
         }
     });
